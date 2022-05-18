dce8eaa633bf6da8e8775869c7a93fd95f6406b6
rayahas <raya.h@campus.technion.ac.il>
2022-05-18 13:32:54 +0300

    add files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+from preprocessors import load_data, extract_feature
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # load RAVDESS dataset, 75% training 25% testing
+    X_train, X_test, y_train, y_test = load_data(test_size=0.25)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
